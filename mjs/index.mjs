313b7dc241c91be367965bd8f9cf4db7d3e8163e
8ima8 <fabianbeltra@MacBook-Pro-de-Fabian.local>
2025-03-18 17:54:39 -0400

    ejemplo de CommonJS y ES6 Modules

---
+++ mjs/index.mjs
@@ -0,0 +1,9 @@
+// .js -> por defecto utiliza common JS
+// .mjs -> por defecto utiliza ES6 ES MODULES
+// .cjs -> por defecto utiliza common JS
+
+import {sum,sub} from './sum.mjs';
+
+console.log(sum(1,2))
+
+console.log(sub(5,2))
