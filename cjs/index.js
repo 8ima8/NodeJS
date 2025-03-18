b34cbca1656d14fa530abb6e954b32dfdb7a6edf
8ima8 <fa.beltransalas@gmail.com>
2025-03-18 17:54:39 -0400

    ejemplo de CommonJS y ES6 Modules

---
+++ cjs/index.js
@@ -0,0 +1,4 @@
+// CommonJS require modulo
+const { sum } = require('./sum');
+
+console.log(sum(1, 2)); // 3
---
+++ cjs/sum.js
@@ -0,0 +1,8 @@
+
+function sum (a,b){
+    return a+b
+}
+//common module export
+module.exports = {
+    sum
+}
